9a52e9948c719e90f7b0f1047ef07a5b2fbe205f
Xuhang Fan <xuhang.fan@duke.edu>
2023-07-15 20:40:33 -0400

    update self-intro

--- .vitepress/theme/components/About.vue
+++ .vitepress/theme/components/About.vue
@@ -4,9 +4,7 @@
       <h2 class="title">About Me</h2>
       <hr>
       <p class = "des">
-      <span >I graduated from Zhejiang University in 2022, and currently I further my study in Economics at Duke University. </span>
-      <span>My research interest includes deception, morality, and belief bias in behavioral research, and market design in marketing research. </span>
-      <span>In recent projects, I utilized both Field/Lab experiments for data generation, and structural modeling for analysis.</span>
+      <span >I graduated from Zhejiang University in 2022, and I am currently pursuing my studies in Economics at Duke University. My research interests include deception, morality, and belief bias in behavioral research, as well as market design in marketing research. In recent projects, I have utilized both Field/Lab experiments for data generation and structural modeling for analysis.</span>
       </p>
       <br>
       <p>Wanna know more about me? Click and check my <b><a href="archives">blogs</a></b> and <b><a href="cv">CV</a></b></p>
